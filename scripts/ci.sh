fcfd382132e4d5a9f48e33b648dd7b69b9cecbc6
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2018-11-28 11:15:22 -0500

    dbus-run-session and xvfb-run?

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -5,6 +5,9 @@ eval $(echo -n "" | /usr/bin/gnome-keyring-daemon --login)
 eval $(/usr/bin/gnome-keyring-daemon --components=secrets --start)
 export GNOME_KEYRING_CONTROL GNOME_KEYRING_PID GPG_AGENT_INFO SSH_AUTH_SOCK
 
-which dbus-launch
+echo "DBUS_SESSION_BUS_ADDRESS: ${DBUS_SESSION_BUS_ADDRESS}"
 
-dbus-launch npm test
+printenv
+
+# https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=836286
+dbus-run-session -- xvfb-run npm test
